831e9fb4d317fee27aa2f30eb95615bef6294197
Alejandro Burgos <alejandroburgos97@yahoo.com>
2023-09-08 18:26:23 -0500

    First commit

---
+++ src/main/java/com/nexossoftware/inventorysystem/NexosSoftwareInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.nexossoftware.inventorysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexosSoftwareInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexosSoftwareInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexossoftware/inventorysystem/NexosSoftwareInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexossoftware.inventorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexosSoftwareInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
